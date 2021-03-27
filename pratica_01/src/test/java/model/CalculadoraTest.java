7ec38a75e5a07d6ba7470b4bf34ced00c56f5290
andresouzaesilva <arsouzaesilva@gmail.com>
2021-03-27 19:01:11 -0300

    Fase Vermelha

---
+++ pratica_01/src/test/java/model/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import model.Calculadora;
+
+public class CalculadoraTest{
+	
+	@Test
+	public void porcentagemTest(){
+		assertEquals(110, resultado);
+	}
+
+}
